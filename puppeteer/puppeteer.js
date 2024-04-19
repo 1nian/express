44235aa00e952badcf834e8d0a7c4c8d838ccca5
zs.yu <zs.yu@herin.ai>
2024-04-19 16:13:31 +0800

    feat:ffmpeg/puppeteer案例

--- puppeteer/puppeteer.js
+++ puppeteer/puppeteer.js
@@ -1,36 +1,56 @@
 import puppeteer from "puppeteer";
+import fs from "node:fs";
 
 // 延迟加载
 const delay = (time) => new Promise((resolve) => setTimeout(resolve, time));
 
-// 获取文本
-const getInnerText = async (el) => {
-    const property = await el.getProperty("innerText");
+// 获取class文本
+const getInnerText = async (el, type = "innerText") => {
+    const property = await el.getProperty(type);
     const text = await property.jsonValue();
     return text;
 };
 
-const init = async () => {
+// 文本存储
+const saveText = async (title, text) => {
+    let texts = title + "\n" + text + "\n" + "\n";
+    fs.appendFile("./text.txt", texts, function (err) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log("The file was saved!");
+    });
+};
+
+const init = async (pageUrl) => {
     // 创建浏览器
-    const browser = await puppeteer.launch({ headless: false });
+    const browser = await puppeteer.launch();
     // 新建页面
     const page = await browser.newPage();
     // 设置宽高
     await page.setViewport({ width: 1920, height: 1080 });
     // 跳转页面
-    await page.goto("https://www.biqg.cc/book/6909/1.html");
+    await page.goto(pageUrl);
     // 等待元素加载完成
     await page.waitForSelector(".content");
     let titleClass = await page.$(".content .wap_none");
     let contentClass = await page.$(".content #chaptercontent");
-    // 获取文本
+    let nextClass = await page.$(".content #pb_next");
+    // 获取title文本
     let title = await getInnerText(titleClass);
-    console.log(title, "----title");
+    // 获取content文本
     let content = await getInnerText(contentClass);
-    console.log(content, "----content");
-    // 截图
-    await page.screenshot({ path: "example.png", fullPage: true });
+    // 获取next文本
+    let next = await getInnerText(nextClass, "href");
+    let nextArr = next.split(".html");
 
-    // await browser.close();
+    // 保存到文件
+    await saveText(title, content);
+
+    if (next && nextArr?.length > 1) {
+        await delay(5000);
+        await init(next);
+    }
+    await browser.close();
 };
-init();
+await init("https://www.biqg.cc/book/6909/1.html");
